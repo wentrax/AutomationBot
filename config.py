12e529e9e4171971494a5fa832e3964776ca158b
WENTRAX <127533917+wentrax@users.noreply.github.com>
2023-04-29 10:43:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,32 +10,16 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
-class Config(object):
-    # Get a bot token from @botfather
-    TG_BOT_TOKEN = "6093418154:AAGs0LQa19RBe9r1pkDizg7Q_OaoCSj4FfQ"
-
-    # Get from my.telegram.org
-    APP_ID = int("16448144")
-
-    # Get from my.telegram.org
-    API_HASH = "1073665850700150caf0e0cbb68216a2"
-
-    # Authorized users to use this bot
-    AUTH_USERS = [5163706369, 1533128706, 1985266909]
-
-    # session name
-    TG_USER_SESSION_NAME = "lx_0980")
-
-    # tg user session string
-    TG_USER_SESSION_STRING = ""
-
-    # your channel id 
-    FILTER_CHANNEL_ID = -1001922400671
-
-    # file forward channel ID
+class Config(object):    
+    TG_BOT_TOKEN = "6093418154:AAGs0LQa19RBe9r1pkDizg7Q_OaoCSj4FfQ"   
+    APP_ID = int("16448144")    
+    API_HASH = "1073665850700150caf0e0cbb68216a2"    
+    AUTH_USERS = [5163706369, 1533128706, 1985266909]  
+    TG_USER_SESSION_NAME = "lx_0980")   
+    TG_USER_SESSION_STRING = ""   
+    FILTER_CHANNEL_ID = -1001922400671  
     FILES_FROM_CHANNEL = -1001739094949
     FILES_TO_CHANNEL = -1001506021749
-    
-        
+            
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
