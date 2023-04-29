620ef23c4635bfafb6b3b243e6ddac7ff23e9b93
WENTRAX <127533917+wentrax@users.noreply.github.com>
2023-04-29 10:27:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,25 +12,25 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
     # Get a bot token from @botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = "5910326190:AAGJ4GkXP4uEwCMGSvaMBZpqB7nYqOIYHoE"
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int("16448144")
 
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = "1073665850700150caf0e0cbb68216a2"
 
     # Authorized users to use this bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = [5163706369, 1533128706, 1985266909]
 
     # session name
-    TG_USER_SESSION_NAME = os.environ.get("TG_USER_SESSION_NAME", "oxmohsen")
+    TG_USER_SESSION_NAME = "lx_0980")
 
     # tg user session string
-    TG_USER_SESSION_STRING = os.environ.get("TG_USER_SESSION_STRING", "")
+    TG_USER_SESSION_STRING = ""
 
     # your channel id 
-    FILTER_CHANNEL_ID = int(os.environ.get("FILTER_CHANNEL_ID", ""))
+    FILTER_CHANNEL_ID = -1001922400671
 
     # file forward channel ID
     FILES_FROM_CHANNEL = -1001739094949
